551e53bd9c765880814f995c29e7b861d0d92553
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-31 05:38:34 +0000

    Day 12: scope

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 ################### Scope ####################
 
+# enemies = 1
+
+# def increase_enemies():
+#   enemies = 2
+#   print(f"enemies inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemies outside function: {enemies}")
+
+#Local Scope
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+
+# drink_potion()
+# print(potion_strength)
+
+
+#Global scope
+# player_health = 10
+
+# def drink_potion():
+#     potion_strength = 2
+#     print(player_health)
+
+# drink_potion()
+
+#There is no block scope
+# game_level = 3
+# enemies = ["Skeleton", "Zombie", "Alien"]
+
+# def create_enemy():
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+
+# print(new_enemy)
+
+#Modifyng global scope
 enemies = 1
 
 def increase_enemies():
-  enemies = 2
   print(f"enemies inside function: {enemies}")
+  return enemies + 1
 
-increase_enemies()
+enemies = increase_enemies()
 print(f"enemies outside function: {enemies}")
